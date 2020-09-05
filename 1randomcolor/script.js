003bdedd1d589297f968e9ce27953fc856264c98
BookShell527 <kurniajijagad@gmail.com>
2020-09-05 19:03:27 +0700

    adding random color

--- 1randomcolor/script.js
+++ 1randomcolor/script.js
@@ -3,16 +3,25 @@ const btn = document.querySelector(".btn")
 const html = document.querySelector("html")
 const mode = document.querySelector(".mode")
 
+// variables needed
+let b1 = 255;
+let b2 = 255;
+let b3 = 255;
+let bColor = color.innerHTML;
+
 // get random value
 function random() {
     return Math.ceil(Math.random()*200)
 }
 
 // change color
-btn.addEventListener("click", e => {
-    const rgb = `rgb(${random()}, ${random()}, ${random()})`
-    color.innerHTML = rgb;
-    html.style.backgroundColor = rgb;
+btn.addEventListener("click", () => {
+    b1 = random();
+    b2 = random();
+    b3 = random();
+    bColor = `rgb(${b1}, ${b2}, ${b3})`
+    color.innerHTML = bColor;
+    html.style.backgroundColor = bColor;
 })
 
 // function to convert rgb to hex
@@ -25,3 +34,14 @@ function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
+mode.addEventListener("click", () => {
+    if (mode.innerHTML === "rgb") {
+        mode.innerHTML = "hex";
+        color.innerHTML = rgbToHex(b1,b2,b3);
+        return;
+    } else if (mode.innerHTML === "hex") {
+        color.innerHTML = bColor;
+        mode.innerHTML = "rgb";
+        return;
+    }
+})
